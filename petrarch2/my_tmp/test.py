dcede3f2831bd3b9cdd0fd3fbaa937b19eb530f6
Ruslan Fayzrakhmanov <ruslanrf@gmail.com>
2016-06-13 13:29:43 +0100

    minor

--- petrarch2/my_tmp/test.py
+++ petrarch2/my_tmp/test.py
@@ -70,7 +70,7 @@ def extract_event_1():
 
     pp = pprint.PrettyPrinter(indent=4)
 
-    s_parsed = False
+    s_parsed = True
     events = PETRreader.read_xml_input(params['paths'], s_parsed)
 
     print ('PARSED INPUT:')
@@ -80,9 +80,9 @@ def extract_event_1():
     if not s_parsed:
         events = stanford_parse(events)
 
-    print ('PARSED INPUT 2:')
-    pp.pprint (events)
-    print ('=====   =====')
+    # print ('PARSED INPUT 2:')
+    # pp.pprint (events)
+    # print ('=====   =====')
 
     # out = ""  # PETRglobals.EventFileName
     out = 'test.out.txt'
@@ -105,50 +105,50 @@ def hypnos_parse():
 # hypnos_parse()
 
 
-import corenlp
+# import corenlp
 
-def stanford_parse(event_dict):
-    logger = logging.getLogger('petr_log')
-    # What is dead can never die...
-    print("\nSetting up StanfordNLP. The program isn't dead. Promise.")
-    logger.info('Setting up StanfordNLP')
-    core = corenlp.StanfordCoreNLP(PETRglobals.stanfordnlp,
-                                   properties=_get_data('data/config/',
-                                                        'petrarch.properties'),
-                                   memory='2g')
-    total = len(list(event_dict.keys()))
-    print(
-        "Stanford setup complete. Starting parse of {} stories...".format(total))
-    logger.info(
-        'Stanford setup complete. Starting parse of {} stories.'.format(total))
-    for i, key in enumerate(event_dict.keys()):
-        if (i / float(total)) * 100 in [10.0, 25.0, 50, 75.0]:
-            print('Parse is {}% complete...'.format((i / float(total)) * 100))
-        for sent in event_dict[key]['sents']:
-            logger.info('StanfordNLP parsing {}_{}...'.format(key, sent))
-            sent_dict = event_dict[key]['sents'][sent]
+# def stanford_parse(event_dict):
+#     logger = logging.getLogger('petr_log')
+#     # What is dead can never die...
+#     print("\nSetting up StanfordNLP. The program isn't dead. Promise.")
+#     logger.info('Setting up StanfordNLP')
+#     core = corenlp.StanfordCoreNLP(PETRglobals.stanfordnlp,
+#                                    properties=_get_data('data/config/',
+#                                                         'petrarch.properties'),
+#                                    memory='2g')
+#     total = len(list(event_dict.keys()))
+#     print(
+#         "Stanford setup complete. Starting parse of {} stories...".format(total))
+#     logger.info(
+#         'Stanford setup complete. Starting parse of {} stories.'.format(total))
+#     for i, key in enumerate(event_dict.keys()):
+#         if (i / float(total)) * 100 in [10.0, 25.0, 50, 75.0]:
+#             print('Parse is {}% complete...'.format((i / float(total)) * 100))
+#         for sent in event_dict[key]['sents']:
+#             logger.info('StanfordNLP parsing {}_{}...'.format(key, sent))
+#             sent_dict = event_dict[key]['sents'][sent]
 
-            if len(sent_dict['content']) > 512 or len(
-                    sent_dict['content']) < 64:
-                logger.warning(
-                    '\tText length wrong. Either too long or too short.')
-                pass
-            else:
-                try:
-                    stanford_result = core.raw_parse(sent_dict['content'])
-                    s_parsetree = stanford_result['sentences'][0]['parsetree']
-                    if 'coref' in stanford_result:
-                        sent_dict['coref'] = stanford_result['coref']
+#             if len(sent_dict['content']) > 512 or len(
+#                     sent_dict['content']) < 64:
+#                 logger.warning(
+#                     '\tText length wrong. Either too long or too short.')
+#                 pass
+#             else:
+#                 try:
+#                     stanford_result = core.raw_parse(sent_dict['content'])
+#                     s_parsetree = stanford_result['sentences'][0]['parsetree']
+#                     if 'coref' in stanford_result:
+#                         sent_dict['coref'] = stanford_result['coref']
 
-                    # TODO: To go backwards you'd do str.replace(' ) ', ')')
-                    sent_dict['parsed'] = _format_parsed_str(s_parsetree)
-                except Exception as e:
-                    print('Something went wrong. See log file.')
-                    logger.warning(
-                        'Error on {}_{}. {}'.format(key, sent, e))
-    print('Done with StanfordNLP parse...\n\n')
-    logger.info('Done with StanfordNLP parse.')
+#                     # TODO: To go backwards you'd do str.replace(' ) ', ')')
+#                     sent_dict['parsed'] = _format_parsed_str(s_parsetree)
+#                 except Exception as e:
+#                     print('Something went wrong. See log file.')
+#                     logger.warning(
+#                         'Error on {}_{}. {}'.format(key, sent, e))
+#     print('Done with StanfordNLP parse...\n\n')
+#     logger.info('Done with StanfordNLP parse.')
 
-    return event_dict
+#     return event_dict
 
 extract_event_1()
